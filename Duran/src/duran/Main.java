0670110615621e6c732077765c50f120eb3a8673
o_0 <o_0@emmanuel.mx>
2011-11-12 23:09:32 +0000

    Creacion del proyecto

---
+++ Duran/src/duran/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package duran;
+
+/**
+ *
+ * @author emmanuel
+ */
+public class Main {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+}
