d945ce77c4e5dd95319e1d523a6512e5b0e05a49
seguidor777@gmail.com <seguidor777@gmail.com@136b548c-b339-2722-0106-e86f507fe506>
2011-11-13 01:34:00 +0000

    Se reemplazó la clase Main por Menu con una interfaz gráfica

---
+++ trunk/Duran/src/duran/Menu.java
@@ -0,0 +1,94 @@
+package duran;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Menu {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Menu window = new Menu();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Menu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 717, 409);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(Menu.class.getResource("/img/duran logo.png")));
+		label.setBounds(12, 12, 249, 141);
+		frame.getContentPane().add(label);
+		
+		JLabel lblSistemaDeVentas = new JLabel("Sistema de ventas, cotizaciones");
+		lblSistemaDeVentas.setFont(new Font("Dialog", Font.BOLD, 20));
+		lblSistemaDeVentas.setHorizontalAlignment(SwingConstants.CENTER);
+		lblSistemaDeVentas.setBounds(273, 25, 420, 50);
+		frame.getContentPane().add(lblSistemaDeVentas);
+		
+		JLabel lblYControlDe = new JLabel("y control de inventario");
+		lblYControlDe.setFont(new Font("Dialog", Font.BOLD, 20));
+		lblYControlDe.setHorizontalAlignment(SwingConstants.CENTER);
+		lblYControlDe.setBounds(273, 74, 420, 50);
+		frame.getContentPane().add(lblYControlDe);
+		
+		JLabel lblMenDeOpciones = new JLabel("Menú de opciones");
+		lblMenDeOpciones.setFont(new Font("Dialog", Font.BOLD, 18));
+		lblMenDeOpciones.setBounds(273, 191, 171, 15);
+		frame.getContentPane().add(lblMenDeOpciones);
+		
+		JButton btnRealizar = new JButton("Realizar Venta");
+		btnRealizar.setBounds(30, 245, 171, 50);
+		frame.getContentPane().add(btnRealizar);
+		
+		JButton btnRealizarCotizacion = new JButton("Realizar Cotización");
+		btnRealizarCotizacion.setBounds(273, 245, 164, 50);
+		frame.getContentPane().add(btnRealizarCotizacion);
+		
+		JButton btnConsultarInventario = new JButton("Consultar Inventario");
+		btnConsultarInventario.setBounds(510, 245, 171, 50);
+		frame.getContentPane().add(btnConsultarInventario);
+		
+		JButton btnSalir = new JButton("Salir");
+		btnSalir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		btnSalir.setIcon(new ImageIcon(Menu.class.getResource("/com/sun/java/swing/plaf/gtk/resources/gtk-cancel-4.png")));
+		btnSalir.setBounds(302, 341, 107, 25);
+		frame.getContentPane().add(btnSalir);
+	}
+}
