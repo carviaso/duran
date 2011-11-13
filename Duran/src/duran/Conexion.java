b3348c548e63b40ad49f887d5d1f39313623167d
seguidor777 <seguidor777@gmail.com>
2011-11-13 09:29:34 +0000

    Conexión a la base de datos funcionando

--- Duran/src/duran/Conexion.java
+++ Duran/src/duran/Conexion.java
@@ -3,7 +3,27 @@ package duran;
 /**
  * @author emmanuel
  */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Conexion {
+	
+	private static Connection con;
+	private static Statement st;
+	
+	public Conexion() {
+		try {
+			con = DriverManager.getConnection("jdbc:mysql://localhost/duran", "root", "root");
+			st = con.createStatement();
+		} catch (SQLException e) {
+			System.out.println("SQLException: " + e.getMessage());
+		    System.out.println("SQLState: " + e.getSQLState());
+		    System.out.println("VendorError: " + e.getErrorCode());
+		}
+	}
 	
 	public int getNoProducto(String nombreProducto) {
 		int noProducto = -1;
@@ -27,6 +47,16 @@ public class Conexion {
 		precioProducto = 1000.00;
 		
 		return precioProducto;
+	}
+	
+	public void insertarVenta(String nombreCliente, double totalVenta) {
+		//System.out.println("Nombre del cliente "+ nombreCliente + ", total " + total);
+		try {
+			st.executeUpdate("INSERT INTO ventas (nombrecliente, totalventa) VALUES('" + nombreCliente + "', '" + totalVenta + "')");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
