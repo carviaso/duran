675714f04887e2c7f9bf8c2ee524e1ac1cd1ef8e
seguidor777 <seguidor777@gmail.com>
2011-11-13 21:21:03 +0000

    Formulario de cotizaciones creado con funcionalidad y todo

--- Duran/src/duran/Conexion.java
+++ Duran/src/duran/Conexion.java
@@ -6,13 +6,16 @@ package duran;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Vector;
 
 public class Conexion {
 	
 	private static Connection con;
-	private static Statement st;
+	private Statement st;
+	private ResultSet rs;
 	
 	public Conexion() {
 		try {
@@ -25,34 +28,122 @@ public class Conexion {
 		}
 	}
 	
-	public int getNoProducto(String nombreProducto) {
-		int noProducto = -1;
+	public Vector getDatosProducto(String nombreProducto) {
+		int noProducto = 0;
+		String nombreRealProducto = null;
+		Vector resultado = new Vector();
 		
-		noProducto = 1;
+		try {
+			rs = st.executeQuery("SELECT idproducto, nombreproducto FROM inventario WHERE nombreproducto LIKE '%" + nombreProducto + "%'");
+			if(rs.first()) {
+			    noProducto = rs.getInt("idproducto");
+			    nombreRealProducto = rs.getString("nombreproducto");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		return noProducto;
+		resultado.add(noProducto);
+		resultado.add(nombreRealProducto);
+		return resultado;
 	}
 	
 	public String getNombreProducto(int noProducto) {
-		String nombreProducto = "";
+		String nombreProducto = null;
 		
-		nombreProducto = "Lámina";
+		try {
+			rs = st.executeQuery("SELECT nombreproducto FROM inventario WHERE idproducto = " + noProducto);
+			if(rs.first()) {
+			    nombreProducto = rs.getString("nombreproducto");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 		
 		return nombreProducto;
 	}
 	
 	public double getPrecioProducto(int noProducto) {
-		double precioProducto;
+		double precioProducto = 0;
 		
-		precioProducto = 1000.00;
+		try {
+			rs = st.executeQuery("SELECT precioporkilo FROM inventario WHERE idproducto = " + noProducto);
+			if(rs.first()) {
+			    precioProducto = rs.getDouble("precioporkilo");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
 		
 		return precioProducto;
 	}
 	
-	public void insertarVenta(String nombreCliente, double totalVenta) {
-		//System.out.println("Nombre del cliente "+ nombreCliente + ", total " + total);
+	public int getCantidadDisponible(int noProducto) {
+		int cantidad = 0;
+		
 		try {
-			st.executeUpdate("INSERT INTO ventas (nombrecliente, totalventa) VALUES('" + nombreCliente + "', '" + totalVenta + "')");
+			rs = st.executeQuery("SELECT cantidad FROM inventario WHERE idproducto = " + noProducto);
+			if(rs.first()) {
+			    cantidad = rs.getInt("cantidad");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return cantidad;
+	}
+	
+	public void retirarDeInventario(int noProducto, int cantidad) {
+		try {
+			st.executeUpdate("UPDATE inventario SET cantidad = cantidad - " + cantidad + " WHERE idproducto = " + noProducto);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public int insertarVenta(String nombreCliente, double totalVenta) {
+		int idventa = -1;
+		
+		try {
+			idventa = st.executeUpdate("INSERT INTO ventas (nombrecliente, totalventa) VALUES('" + nombreCliente + "', " + totalVenta + ")", Statement.RETURN_GENERATED_KEYS);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return idventa;
+	}
+	
+	public void insertarVentaProducto(int idventa, int idproducto, int cantidad) {
+		try {
+			st.executeUpdate("INSERT INTO ventaproducto (idventa, idproducto, cantidad) VALUES(" + idventa + ", " + idproducto + ", " + cantidad + ")");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public int insertarCotizacion(String nombreCliente, double totalCotizacion) {
+		int idcotizacion = -1;
+		
+		try {
+			idcotizacion = st.executeUpdate("INSERT INTO ventas (nombrecliente, totalventa) VALUES('" + nombreCliente + "', " + totalCotizacion + ")", Statement.RETURN_GENERATED_KEYS);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return idcotizacion;
+	}
+	
+	public void insertarCotizacionProducto(int idventa, int idproducto, int cantidad) {
+		try {
+			st.executeUpdate("INSERT INTO contizacionproducto (idventa, idproducto, cantidad) VALUES(" + idventa + ", " + idproducto + ", " + cantidad + ")");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
